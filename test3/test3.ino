19e14dd49eac175d5b959ff2f46d7cbd86d5aed3
Eleanor Tucker <eleanortucker@Eleanors-MacBook-Air-2.local>
2025-01-29 15:01:05 -0600

    fixed typos

--- test3/test3.ino
+++ test3/test3.ino
@@ -4,9 +4,9 @@ LiquidCrystal lcd(12,11,5,4,3,2);
 void setup() {
   // put your setup code here, to run once:
   lcd.begin(16,2); //Initiate the LCD in a 16x2 configuration
-  lcd.print("My name is:")
+  lcd.print("My name is:");
   lcd.setCursor(0,1);
-  lcd.print("Eleanor")
+  lcd.print("Eleanor");
 }
 
 void loop() {
